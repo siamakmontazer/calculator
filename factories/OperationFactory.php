50b9076d66511cdf63df9cded152798d31d09d60
siamak <s.montazer@abo-s.com>
2022-11-20 20:17:03 +0100

    fix: operation class creation

--- factories/OperationFactory.php
+++ factories/OperationFactory.php
@@ -12,7 +12,7 @@ class OperationFactory
     public static function createOperation(string $operator): AbstractOperation
     {
         $operator = !in_array($operator, self::PERMITTED_OPERATIONS) ? 'Null' : self::PERMITTED_OPERATIONS[$operator];
-        $operationClass = self::PERMITTED_OPERATIONS[$operator] . '';
+        $operationClass = "{$operation}Operation";
 
         // Please note that we can also create validation chain or validation builder and use it here to add operation
         // validators according to the operator strategy, for now I kept it simple
